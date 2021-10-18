36f53b7bfc0136a96895d3c9d7ad9339ee993ce7
gabriel <gabriel@example.com>
2021-10-18 14:56:52 -0300

    criando docker

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 cd ~/Desktop/projetos/java-app-deploy-academia-devops
-docker build -t didox/app-nodejs-validacao-cnpj -f Dockerfile .
-docker run -d -p 3000:3000 --name app-nodejs-validacao-cnpj didox/app-nodejs-validacao-cnpj
-docker exec -it app-nodejs-validacao-cnpj npm run test
-docker stop app-nodejs-validacao-cnpj
-docker tag didox/app-nodejs-validacao-cnpj hub.docker.com/r/didox/app-nodejs-validacao-cnpj
-docker push didox/app-nodejs-validacao-cnpj
-docker rm app-nodejs-validacao-cnpj
+docker build -t trevisangabriel/java-app-deploy-academia-devops -f Dockerfile .
+docker run -d -p 3000:3000 --name java-app-deploy-academia-devops trevisangabriel/java-app-deploy-academia-devops
+docker exec -it java-app-deploy-academia-devops npm run test
+docker stop java-app-deploy-academia-devops
+docker tag trevisangabriel/java-app-deploy-academia-devops hub.docker.com/r/trevisangabriel/java-app-deploy-academia-devops
+docker push trevisangabriel/java-app-deploy-academia-devops
+docker rm java-app-deploy-academia-devops
